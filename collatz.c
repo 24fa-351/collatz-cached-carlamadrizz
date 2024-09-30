85d37966f690f1c701dd3d49393613ee557c1c91
carlamadrizz <111922222+carlamadrizz@users.noreply.github.com>
2024-09-29 21:38:18 -0700

    Update collatz.c

--- collatz.c
+++ collatz.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to calculate the number of steps for Collatz conjecture
+//  Collatz conjecture
 int collatz_steps(int n) {
     int steps = 0;
     while (n != 1) {
@@ -11,13 +11,13 @@ int collatz_steps(int n) {
         } else {
             n = 3 * n + 1;  // If odd, multiply by 3 and add 1
         }
-        steps++;  // Increment the step counter
+        steps++;  
     }
     return steps;
 }
 
 int main(int argc, char *argv[]) {
-    // Ensure that the correct number of arguments is provided
+  
     if (argc != 4) {
         printf("Usage: %s <N> <MIN> <MAX>\n", argv[0]);
         return 1;
@@ -28,19 +28,19 @@ int main(int argc, char *argv[]) {
     int MIN = atoi(argv[2]); // Smallest value to test
     int MAX = atoi(argv[3]); // Largest value to test
 
-    // Validate inputs
+ 
     if (MIN > MAX) {
         printf("Error: MIN should be less than or equal to MAX.\n");
         return 1;
     }
 
-    // Seed the random number generator
+    
     srand(time(NULL));
 
     // Loop to generate and test N random numbers between MIN and MAX
     for (int i = 0; i < N; i++) {
-        int num = rand() % (MAX - MIN + 1) + MIN;  // Generate a random number
-        int steps = collatz_steps(num);            // Calculate Collatz steps
+        int num = rand() % (MAX - MIN + 1) + MIN;  
+        int steps = collatz_steps(num);            
         printf("Number: %d, Steps: %d\n", num, steps);
     }
 
